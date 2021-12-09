f730f31eb03384e09d81ab1b0d444d746f0be5e0
muhamadhashbiashiddiq <muhamadhashbiashiddiq@gmail.com>
2021-12-09 22:02:43 +0700

    Error di node

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 import "./assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
-  return <div className="App">Hai Duniaaaaa!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes path="/" component={LandingPage}></Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
